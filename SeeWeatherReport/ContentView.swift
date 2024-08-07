58ed3f5d8477203cab63394e5fbef733d04bfcbb
Viacheslav Belov <vektorification@gmail.com>
2024-08-07 15:13:03 +0200

    Initial commit

---
+++ SeeWeatherReport/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeeWeatherReport
+//
+//  Created by Viacheslav Belov on 07.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeeWeatherReport/SeeWeatherReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeWeatherReportApp.swift
+//  SeeWeatherReport
+//
+//  Created by Viacheslav Belov on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SeeWeatherReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
