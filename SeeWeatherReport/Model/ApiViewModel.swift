1ff2421f625ed39ed9a8b69d5655e6417d28a077
Viacheslav Belov <vektorification@gmail.com>
2024-08-15 19:30:55 +0200

    created model for neeeded weather parameters

--- SeeWeatherReport/Model/ApiViewModel.swift
+++ SeeWeatherReport/Model/ApiViewModel.swift
@@ -19,7 +19,8 @@ class ApiViewModel: ObservableObject {
         guard let url = URL(string: urlString) else { return }
         let task = session.dataTask(with: url) { (data, response, error) in
             if let data = data {
-                self.parseJSON(withData: data)
+                let currentWeather = self.parseJSON(withData: data)
+                print(currentWeather)
             } else if let error = error {
                 print("Request error: \(error)")
             }
@@ -27,12 +28,17 @@ class ApiViewModel: ObservableObject {
         task.resume()
     }
     
-    func parseJSON (withData data: Data) {
+    func parseJSON (withData data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         do {
             let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
+            guard let currentWeather = CurrentWeather(currentWeatherData: currentWeatherData) else {
+                return nil
+            }
+            return currentWeather
         } catch let error as NSError {
             print(error.localizedDescription)
         }
+        return nil
     }
 }
